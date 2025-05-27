e2e150b013cf90ba8bb51cba12d6be1996269a10
nachocjs <iramos99106@gmail.com>
2025-05-27 19:45:30 -0300

    init

---
+++ src/ProductManager.js
@@ -0,0 +1,96 @@
+import fs from "fs";
+import { parse } from "path";
+
+class ProductManager {
+
+  constructor(pathFile) {
+    this.pathFile = pathFile;
+  }
+
+  async getProducts() {
+    try {
+      const fileData = await fs.promises.readFile(this.pathFile, "utf-8");
+      const products = JSON.parse(fileData);
+      return products;
+    } catch (error) {
+      throw new Error("Error al traer los productos - ", error.message);
+    }
+  }
+
+  generateNewId(products) {
+    if (products.length > 0) {
+      return products[products.length - 1].id + 1;
+    } else {
+      return 1;
+    }
+  }
+
+  async addProduct(newProduct) {
+    try {
+      const fileData = await fs.promises.readFile(this.pathFile, "utf-8");
+      const products = JSON.parse(fileData);
+
+      const newId = this.generateNewId(products);
+      const product = { id: newId, ...newProduct };
+      products.push(product);
+
+      await fs.promises.writeFile(this.pathFile, JSON.stringify(products, null, 2), "utf-8");
+      return products;
+    } catch (error) {
+      throw new Error("Error al añadir el producto - ", error.message);
+    }
+  }
+
+  async deleteProductById(idProduct){
+    try {
+      const fileData = await fs.promises.readFile(this.pathFile, 'utf-8');
+      const data = JSON.parse(fileData);
+      const productIndex = data.findIndex((prod) => prod.id === parseInt(idProduct));
+
+      if (productIndex === -1) throw new Error(`Producto con id: ${idProduct} no encontrado`);
+      data.splice(productIndex, 1);
+
+      await fs.promises.writeFile(this.pathFile, JSON.stringify(data, null, 2), 'utf-8');
+
+      return data;
+    } catch (error) {
+      throw new Error(`Error al eliminar el producto: ${error.message}`);
+    }
+  }
+
+  async updateProductById(idProduct, updatedProduct){
+    try {
+      const fileData = await fs.promises.readFile(this.pathFile, 'utf-8');
+      const data = JSON.parse(fileData);
+      const productIndex = data.findIndex((prod) => prod.id === parseInt(idProduct));
+      if (productIndex === -1) throw new Error(`Producto con id: ${idProduct} no encontrado`);
+
+      data[productIndex] = { ...data[productIndex], ...updatedProduct };
+      await fs.promises.writeFile(this.pathFile, JSON.stringify(data, null, 2), 'utf-8');
+
+      return data;
+    } catch (error) {
+      throw new Error(`Error al actualizar el producto: ${error.message}`);
+    }
+  }
+
+  async getProductById(idProduct){
+    try {
+      const fileData = await fs.promises.readFile(this.pathFile, 'utf-8');
+      const data = JSON.parse(fileData);
+      const findProduct = data.find(product => product.id === parseInt(idProduct))
+
+
+      if (!idProduct || isNaN(idProduct)) {
+            throw new Error(`Producto con id: ${idProduct} no existe`)
+        }
+        
+        return findProduct
+
+    } catch (error) {
+        throw new Error(`Error al buscar el producto: ${error.message}`)
+    }
+  }
+}
+
+export default ProductManager;
---
+++ src/app.js
@@ -0,0 +1,72 @@
+import express from "express";
+import ProductManager from "./ProductManager.js";
+
+const app = express();
+
+app.use(express.json());
+
+const productManager = new ProductManager("./src/products.json");
+
+
+//GET All Products
+app.get("/api/products", async(req, res)=> {
+  try {
+    const products = await productManager.getProducts();
+
+    res.status(200).json({ status: "success", products });
+  } catch (error) {
+    res.status(500).json({ status: "error" });
+  }
+});
+
+//POST Add Product
+app.post("/api/products", async(req, res) => {
+  try {
+    const newProduct = req.body;
+    const products = await productManager.addProduct(newProduct);
+    res.status(201).json({ status : "success", products });
+  } catch (error) {
+    res.status(500).json({ status: "error" });
+  }
+});
+
+//DELETE Product by id
+app.delete("/api/products/:pid", async(req, res)=> {
+  try {
+    const productId = req.params.pid;
+    const products = await productManager.deleteProductById(productId);
+    res.status(200).json({ status: "success", products });
+  } catch (error) {
+    res.status(500).json({ status: "error" });
+  }
+});
+
+//PUT Update Product
+app.put("/api/products/:pid", async(req, res)=> {
+  try {
+    const productId = req.params.pid;
+    const updatedData = req.body;
+
+    const products = await productManager.updateProductById(productId, updatedData);
+    res.status(200).json({ status: "success", products });
+  } catch (error) {
+    res.status(500).json({ status: "error" });
+  }
+});
+
+//GET ProductById
+app.get("/api/products/:pid", async(req, res)=> {
+  try {
+    const productId = req.params.pid;
+    const product = await productManager.getProductById(productId);
+
+    res.status(200).json({ status: "success", product });
+  } catch (error) {
+    res.status(500).json({ status: "error" });
+  }
+});
+
+
+app.listen(8080, ()=> {
+  console.log("Servidor iniciado en el puerto 8080");
+});
